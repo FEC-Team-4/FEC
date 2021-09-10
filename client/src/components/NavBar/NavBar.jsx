bfe56abf197786f7a70047ba8b070f7e4c8a28c4
Cassandra Barragan <cassbarragan@gmail.com>
2021-09-09 19:30:50 -0700

    Updates to file structure, package.json, installed webpack, set up server. Server is running, page is rendering. We should be relatively ready to go.

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css'
+
+export default class NavBar extends React.Component {
+  render() {
+    return (
+      <h1>
+        <a href='#'>Navigation Bar?</a>
+      </h1>
+    );
+  }
+}
