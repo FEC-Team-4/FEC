d128b1f5016d07dc883340a0ef9b8ea85d253cdd
nicholas ma <nicholas.maa@gmail.com>
2021-09-13 21:29:44 -0700

    helpful button is updating now

--- client/src/components/RatingsReviews/components/Review.jsx
+++ client/src/components/RatingsReviews/components/Review.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import axios from 'axios'
+
+const apiKey = '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'
+
 
 class Review extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      feedback: false
+    }
+    this.helpful = this.helpful.bind(this)
   }
 
   ifRec() {
@@ -11,6 +19,27 @@ class Review extends React.Component {
         <p> &#10004; I recommend this product</p>
       )
     }
+  }
+
+  thanks() {
+    if (this.state.feedback) {
+      return (
+        <p>
+          Thanks for your feedback :)
+        </p>
+      )
+    } else {
+      return (
+        <div>
+        <p><i>Helpful? <a href="#" onClick={this.helpful} >Yes</a> ({this.props.review.helpfulness}) </i> || <a href="">Report</a></p>
+        </div>
+      )
+    }
+  }
+
+  helpful() {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/${this.props.review.review_id}/helpful`, {}, {headers: {Authorization: apiKey }})
+    .then(() => this.setState({feedback: true}))
   }
   oneStars() {
     if (this.props.review.rating === 1) {
@@ -68,7 +97,7 @@ class Review extends React.Component {
         <h6>User: {this.props.review.reviewer_name} | {this.props.review.date.split('T')[0]}</h6>
         <h4>{this.props.review.summary}</h4>
         <p className="text-justify">{this.props.review.body}</p>
-        <p><i>Helpful? <a href="">Yes</a> ({this.props.review.helpfulness}) </i> || <a href="">Report</a></p>
+        {this.thanks()}
       </div>
     )
   }
