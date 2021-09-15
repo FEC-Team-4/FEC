069107114afc819d8c15b604396daf477c8aab03
Cassandra Barragan <cassbarragan@gmail.com>
2021-09-15 15:29:39 -0700

    working on isolating data needed for relatedItems.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,32 +10,31 @@ import "./App.css";
 import axios from 'axios';
 import { dataContext } from './context/dataContext.js';
 import token from '../../../token/token.js';
-console.log('datacontext:', dataContext);
-
 
 
 const App = () => {
 
-  // const [items, setItems] = useState(['Shirt', 'pant', 'shoe', 'sock']);
   const [product, setProduct] = useState([]);
   const [reviews, setReviews] = useState([]);
   const [productId, setProductId] = useState(42366);
 
   useEffect(() => {
-    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${productId}/styles`,{params: {count: 50}, headers: {Authorization: token}})
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${productId}`,{params: {count: 50}, headers: {Authorization: token}})
       .then(({data}) => setProduct(data))
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews',{params: {product_id: 42366}, headers: {Authorization: token }})
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews',{params: {product_id: `${productId}`}, headers: {Authorization: token }})
       .then((results) => setReviews(() => results))
   }, [])
-  console.log('reviews from api:', reviews);
-  console.log('product from api:', product);
 
-  //setProductId helper function
+  //setProductId helper function needed
 
-  //producuts:fdiapfsdia
+
 
   return (
-      <dataContext.Provider value={{products}}>
+      <dataContext.Provider value={{
+        product: [product],
+        reviews: [reviews],
+        productId: [productId]
+      }}>
         <Container>
             <NavBar/>
           <div className="ProductDetails">
--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,11 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { dataContext } from '../context/dataContext.js';
 // import './RelatedItems.css'
 // import { Card } from 'react-bootstrap';
 import RelatedItemsCards from './RelatedItemsCards.jsx';
 import YourOutfit from './YourOutfit.jsx';
+import token from '../../../../token/token.js';
+import axios from 'axios';
+
+
 
 
 var RelatedItems = () => {
+  var productId = 42370;
+
+  const { product } = useContext(dataContext);
+  const { reviews } = useContext(dataContext);
+  var { productId } = useContext(dataContext);
+
+  console.log('product', product);
+  console.log('reviews', reviews);
+  console.log('productId', productId);
+
+  const [relatedList, setRelatedList] = useState([]);
+  const [relatedItems, setRelatedItems] = useState({});
+
+  //use context to pull current product ID
+  //pull products related to current item
+    //pull category, name, rating, price, first image for each item.
+      //load that info into the cards
+
+
+  useEffect(() => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${productId}/styles`,{params: {count: 50}, headers: {Authorization: token}})
+      .then(({data}) => setRelatedList(data))
+      .then(() => console.log('results:', results))
+      .catch((err) => console.log('related items ajax err:', err))
+  }, []);
+
+  // getAvgStars () {
+  //   var total = 0;
+  //   relatedItems.forEach(item => {
+  //     total += item.rating
+  //   })
+  //   setRelatedItems({avgStars: total/this.state.reviews.length})
+  // }
+
+  console.log('related list from relateditems:', relatedList.results);
+
   return (
     <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
       <h3>Related Items</h3>
